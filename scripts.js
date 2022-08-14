c135861f831d30711337ea0e515a97960055d684
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-08-13 22:59:06 -0700

    Home page styling

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
+// -----------Style/Cards-----------
+const panels = document.querySelectorAll('.panel'); //puttlin all panels into nodelist
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses(); //function (below) removes all the active classes from other panels that are active
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
+
 // -----------GLOBAL VARIABLES-----------
 
 const displayContainer = document.querySelector('.display');
@@ -5,7 +21,5 @@ const displayContainer = document.querySelector('.display');
 /////////////////////////////////////////////////////
 // -----------Image of the Day Script-----------
 
-
 //////////////////////////////////////////////////////
 // -----------Highlights Script-----------
-
