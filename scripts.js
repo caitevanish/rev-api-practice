e24a5e0a64f3489f91fb8d31c4d90e67f7be07af
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-08-13 20:00:05 -0700

    Moved all scripts to their own html pages (Will refactor onto .js file later)

--- scripts.js
+++ scripts.js
@@ -1,34 +1,11 @@
+// -----------GLOBAL VARIABLES-----------
+
 const displayContainer = document.querySelector('.display');
 
-//Old school AJAX request
+/////////////////////////////////////////////////////
+// -----------Image of the Day Script-----------
 
-const getImageData = function (artObjectId) {
-  const request = new XMLHttpRequest();
-  request.open(
-    'GET',
-    `https://collectionapi.metmuseum.org/public/collection/v1/objects/${artObjectId}`
-  );
-  request.send();
 
-  //once data arrives...
-  //register a callback on the load:
-  request.addEventListener('load', function () {
-    // console.log(this.responseText);
+//////////////////////////////////////////////////////
+// -----------Highlights Script-----------
 
-    const data = JSON.parse(this.responseText);
-    console.log(data);
-
-    const html = `<div>
-      <h3 id="artTitle">${data.title}</h3>
-      <img id="artMainImage" src="${data.primaryImageSmall}" alt="artwork" />
-      <h4 id="artistName">${data.artistDisplayName}</h4>
-      <p id="dim">${data.dimensions}</p>
-      <p id="medium">${data.medium}</p>
-      <p id="classif">${data.classification}</p>
-      <p id="nation">${data.artistNationality}</p>
-      <a id="wikiLink" href="">${data.objectWikidata_URL}</a>
-    </div> `;
-    displayContainer.insertAdjacentHTML('beforeend', html);
-  });
-};
-getImageData('436533');
